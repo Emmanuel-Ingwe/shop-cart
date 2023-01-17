1fe95308cd2d19ed6d924f5ae8a92b280db9a6fd
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-17 23:37:51 +0100

    modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,45 @@
 import { Button, Container, Navbar, Modal } from "react-bootstrap";
+import { useState } from "react";
 
 const NavbarComponent = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <Navbar expand="sm">
-            <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-                <Button>Cart 0 Items</Button>
-            </Navbar.Collapse>
-        </Navbar>
+        <>
+            <Navbar expand="sm">
+                <Navbar.Brand href="/">Ecommerce Store</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                    <Button>Cart 0 Items</Button>
+                </Navbar.Collapse>
+            </Navbar>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Shopping Cart</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h1>This</h1>
+                    {/* {productsCount > 0 ?
+                        <>
+                            <p>Items in your cart:</p>
+                            {cart.items.map((currentProduct, idx) => (
+                                <CartProduct key={idx} id={currentProduct.id} quantity={currentProduct.quantity}></CartProduct>
+                            ))}
+
+                            <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
+
+                            <Button variant="success" onClick={checkout}>
+                                Purchase items!
+                            </Button>
+                        </>
+                        :
+                        <h1>There are no items in your cart!</h1>
+                    } */}
+                </Modal.Body>
+            </Modal>
+        </>
     );
 };
 
