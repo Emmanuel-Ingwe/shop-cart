b0efa59794c71b5f3e61529256cee17319183d7d
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-17 07:06:43 +0100

    pcard

--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { productsArray } from '../productsStore';
+// import ProductCard from '../components/ProductCard';
+// [{... }, {... }, {... }]
+function Store() {
 
-const Store = () => {
     return (
-        <h1>Store</h1>
+        <>
+            <h1 align="center" className="p-3">Welcome to the store!</h1>
+            <Row xs={1} md={3} className="g-4">
+                {productsArray.map((product, idx) => (
+                    <Col align="center" key={idx}>
+                        {/* <ProductCard product={product} /> */}
+                    </Col>
+                ))}
+            </Row>
+        </>
     );
-};
+}
 
 export default Store;
