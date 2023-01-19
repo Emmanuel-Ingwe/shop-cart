a2f610a7d4749bf47e6a1f1f2ef8bc615669d064
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-19 08:25:07 +0100

    form

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -13,7 +13,19 @@ const ProductCard = (props) => {
             <Card.Body>
                 <Card.Title>{product.title}</Card.Title>
                 <Card.Text>${product.price}</Card.Text>
-                <Button variant="primary"></Button>
+                {productQuantity > 0 ?
+                    <>
+                        <Form as={Row}>
+                            <Form.Label column="true" sm="6">In Cart: {product.id}</Form.Label>
+                            <Col sm="6">
+                                <Button onClick={() => cart.addOneToCart(product.id)} className="mx-2" >+</Button>
+                                <Button sm="6" className="mx-2">-</Button>
+                            </Col>
+                        </Form>
+                        <Button variant="danger" onClick={() => cart.deleteFromCart(product.id)} className="my-2">Remove from cart</Button>
+                    </> :
+                    <Button variant="primary">Add To Cart</Button>
+                }
             </Card.Body>
         </Card>
     );
