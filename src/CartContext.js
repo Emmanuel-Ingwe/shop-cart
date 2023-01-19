f764507dbd9e204d974a6678ede7225c62e596db
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-19 07:18:52 +0100

    cart

--- src/CartContext.js
+++ src/CartContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState } from "react";
-import { productsArray } from "./productsStore";
+import { productsArray, getProductData } from "./productsStore";
+
 
 export const CartContext = createContext({
     items: [],
@@ -11,9 +12,86 @@ export const CartContext = createContext({
 });
 
 export function CartProvider({ children }) {
+    const [cartProducts, setCartProducts] = useState([]);
+
+    // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]
+
+    function getProductQuantity(id) {
+        const quantity = cartProducts.find(product => product.id === id)?.quantity;
+
+        if (quantity === undefined) {
+            return 0;
+        }
+
+        return quantity;
+    }
+
+    function addOneToCart(id) {
+        const quantity = getProductQuantity(id);
+
+        if (quantity === 0) { // product is not in cart
+            setCartProducts(
+                [
+                    ...cartProducts,
+                    {
+                        id: id,
+                        quantity: 1
+                    }
+                ]
+            );
+        } else { // product is in cart
+            // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]    add to product id of 2
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                        product.id === id                                // if condition
+                            ? { ...product, quantity: product.quantity + 1 } // if statement is true
+                            : product                                        // if statement is false
+                )
+            );
+        }
+    }
+
+    function removeOneFromCart(id) {
+        const quantity = getProductQuantity(id);
+
+        if (quantity == 1) {
+            deleteFromCart(id);
+        } else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                        product.id === id                                // if condition
+                            ? { ...product, quantity: product.quantity - 1 } // if statement is true
+                            : product                                        // if statement is false
+                )
+            );
+        }
+    }
+
+    function deleteFromCart(id) {
+        // [] if an object meets a condition, add the object to array
+        // [product1, product2, product3]
+        // [product1, product3]
+        setCartProducts(
+            cartProducts =>
+                cartProducts.filter(currentProduct => {
+                    return currentProduct.id != id;
+                })
+        );
+    }
+
+    function getTotalCost() {
+        let totalCost = 0;
+        cartProducts.map((cartItem) => {
+            const productData = getProductData(cartItem.id);
+            totalCost += (productData.price * cartItem.quantity);
+        });
+        return totalCost;
+    }
 
     const contextValue = {
-        items: [],
+        items: cartProducts,
         getProductQuantity,
         addOneToCart,
         removeOneFromCart,
@@ -27,3 +105,5 @@ export function CartProvider({ children }) {
         </CartContext.Provider>
     );
 }
+
+export default CartProvider;
